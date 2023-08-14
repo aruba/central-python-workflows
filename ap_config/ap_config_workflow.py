63425a69febe0d62a714afd647a1db0b8ab77345
Collin <collin.koss@hpe.com>
2023-08-14 16:31:29 -0700

    Updated documentation, implemented functionality to replace entire AP config.

--- ap_config/ap_config_workflow.py
+++ ap_config/ap_config_workflow.py
@@ -3,12 +3,11 @@
 # Copyright (c) 2023 Aruba, a Hewlett Packard Enterprise company
 
 """
-Workflow . This includes creation
-of an ArubaCentralBase instance through the usage of a config file, which is
-required for pycentral API usage. SSID details should be pre-configured in a
-YAML file to be used by the workflow. Please refer to the README for full
-configuration file details.  The workflow will create a new SSID configured to
-the YAML files settings.
+Workflow replaces existing Central AP configurations with input from a .txt
+file.  This includes creation of an ArubaCentralBase instance through the
+usage of a config file, which is required for pycentral API usage.
+Configuration file should contain new AP configuration in CLI format. Please
+refer to the README for full details.
 """
 from pycentral.workflows.workflows_utils import get_conn_from_file
 from ap_cli import ApCLIConfig
@@ -28,14 +27,18 @@ def main():
     with open(args.cli_path) as in_file:
         input_cli = in_file.read().splitlines()
     target = args.ap
-
     ap = ApCLIConfig()
-    # Get existing CLI and merge with input.
-    ap_cli = ap.get_ap_config(central, target)
-    # Format data for post.
-    post_data = {"clis": ap.merge_config(ap_cli, input_cli)}
-    # Post merged config to target.
-    ap.replace_config(central, target, post_data)
+
+    if not args.r:
+        # Get existing CLI.
+        ap_cli = ap.get_ap_config(central, target)
+        # Merge CLI with input, format data for post.
+        post_data = {"clis": ap.merge_config(ap_cli, input_cli)}
+        # Post merged config to target.
+        ap.replace_config(central, target, post_data)
+    else:
+        post_data = {"clis": input_cli}
+        ap.replace_config(central, target, post_data)
 
 
 def define_arguments():
@@ -56,6 +59,8 @@ def define_arguments():
 
     parser.add_argument('ap', metavar='target', type=str,
                         help=('Central groupname or AOS10 AP serial'))
+    parser.add_argument('-r', help="Replace entire configuration",
+                        action='store_true')
     parser.add_argument('--central_auth', default='central_token.yaml',
                         help=('Central Credential auth filepath'))
     parser.add_argument('--cli_path', help=('CLI config filepath'),
